7a8e123637407845aef922488462174238943b5b
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-14 11:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Check if the input string is empty or does not contain any space
+  if (str.trim() === '' || !str.includes(' ')) {
+    return str;
+  }
+
+  // Extract the first word up to the first space
+  return str.split(' ')[0];
 }
 
 // Do not change the code below
